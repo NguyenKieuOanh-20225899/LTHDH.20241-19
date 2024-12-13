df89432e5c184adff6788d84ee69bf7c9f8713bc
Hoang Phong <hoangphong210703@gmail.com>
2024-12-14 01:07:47 +0700

    Create List.java

---
+++ List.java
@@ -0,0 +1,28 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+class List extends DataStructure {
+    @Override
+    public void insert(int value) {
+        elements.add(value);
+    }
+
+    @Override
+    public void delete(int value) {
+        elements.remove((Integer) value);
+    }
+
+    @Override
+    public boolean find(int value) {
+        return elements.contains(value);
+    }
+
+    @Override
+    public void sort() {
+        Collections.sort(elements);
+    }
+}
