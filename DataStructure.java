a283c14ba10b68d3a5bf594c7f504359b2ed8b2e
Hoang Phong <hoangphong210703@gmail.com>
2024-12-14 00:33:09 +0700

    Abstract Class DataStructure

---
+++ DataStructure.java
@@ -0,0 +1,23 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+abstract class DataStructure{
+    protected java.util.List<Integer> elements;
+
+    public DataStructure() {
+        elements = new ArrayList<>();
+    }
+
+    public abstract void insert(int value);
+    public abstract void delete(int value);
+    public abstract boolean find(int value);
+    public abstract void sort();
+
+    public java.util.List<Integer> getElements() {
+        return elements;
+    }
+}
