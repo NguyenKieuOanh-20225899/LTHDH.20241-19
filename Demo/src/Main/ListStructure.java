6dc4ccbbed3a73d174013c59f45b9040aa871e49
NguyenThiNhung <140055464+KangSohyun93@users.noreply.github.com>
2024-12-16 00:08:07 +0700

    Update ListStructure.java

--- Demo/src/Main/ListStructure.java
+++ Demo/src/Main/ListStructure.java
@@ -5,17 +5,14 @@ import java.util.List;
 
 public class ListStructure<T extends Comparable<T>> extends DataStructure<T> {
 
-    // Add an element to the list
     public void add(T item) {
         elements.add(item);
     }
 
-    // Remove an element from the list
     public void remove(T item) {
         elements.remove(item);
     }
 
-    // Get an element by its index
     public T get(int index) {
         if (index < 0 || index >= elements.size()) {
             return null;
@@ -23,21 +20,18 @@ public class ListStructure<T extends Comparable<T>> extends DataStructure<T> {
         return elements.get(index);
     }
 
-    // Sort the list in ascending order
     @Override
     public void sort() {
         Collections.sort((List<T>) elements);
     }
 
-    // Override find() to check if the element exists
     @Override
     public boolean find(T item) {
-        return elements.contains(item); // Return true if the element exists
+        return elements.contains(item); 
     }
 
-    // New method: Find the index of an item
     public int findIndex(T item) {
-        return elements.indexOf(item); // Return the index, or -1 if not found
+        return elements.indexOf(item); 
     }
 
     @Override
